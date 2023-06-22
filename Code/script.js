fe5087457528f4327e19b87e9ea361f4f5606ae6
Atta ul Noor <attaulnoor2001@gmail.com>
2023-06-23 00:13:30 +0500

    Modified

--- Code/script.js
+++ Code/script.js
@@ -35,51 +35,25 @@ if (cart_unslide) {
     })
 }
 
-let addTocart = document.querySelectorAll('.prod .cart');
-let prodname = document.querySelectorAll('.description h5');
-let prodprice = document.querySelectorAll('.description h4');
-let prodimg = document.querySelectorAll('.prod img');
-let cartprodname = document.querySelector('#cart-prod-name');
-let cartprodprice = document.querySelector('#cart-prod-price');
-let cartprodimg = document.querySelector('.items img');
 
-prodname.forEach(function(element) {
-  console.log(element.innerHTML);
-});
-
-prodprice.forEach(function(element) {
-  console.log(element.innerHTML);
-});
-
-console.log(cartprodname.innerHTML);
-console.log(cartprodprice.innerHTML);
-
-addTocart.forEach(function(element) {
-  element.addEventListener('click', () => {
-    let index = Array.from(addTocart).indexOf(element);
-    cartprodname.innerHTML = prodname[index].innerHTML;
-    cartprodprice.innerHTML = prodprice[index].innerHTML;
-    cartprodimg.src = prodimg[index].src;
-  });
-});
 
 
 // Single Product Slider
-let mainImg = document.querySelector(".mainImg");
-let smallImg = document.querySelectorAll(".smallImg");
+// let mainImg = document.querySelector(".mainImg");
+// let smallImg = document.querySelectorAll(".smallImg");
 
-smallImg[0].onclick = function () {
-    mainImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = function () {
-    mainImg.src = smallImg[1].src;
-}
-smallImg[2].onclick = function () {
-    mainImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = function () {
-    mainImg.src = smallImg[3].src;
-}
+// smallImg[0].onclick = function () {
+//     mainImg.src = smallImg[0].src;
+// }
+// smallImg[1].onclick = function () {
+//     mainImg.src = smallImg[1].src;
+// }
+// smallImg[2].onclick = function () {
+//     mainImg.src = smallImg[2].src;
+// }
+// smallImg[3].onclick = function () {
+//     mainImg.src = smallImg[3].src;
+// }
 
 
 // Quantity Increase of the Product
@@ -122,7 +96,7 @@ if (minus) {
 }
 
 
-// Quantity Increase of the Product
+// Size Options of the Product
 let options = document.querySelectorAll(".size option");
 if (options) {
     options.forEach((option) => {
@@ -140,7 +114,7 @@ if (options) {
                 event.target.style.fontWeight = '500';
                 event.target.classList.remove('selected');
                 // console.log("Unclicked option");
-            } 
+            }
             else {
                 event.target.style.color = '#088178';
                 event.target.style.fontWeight = '700';
@@ -151,5 +125,78 @@ if (options) {
     });
 }
 
+// Adding items to cart
+let addTocart = document.querySelectorAll('.prod .cart');
+let prodname = document.querySelectorAll('.description h5');
+let prodprice = document.querySelectorAll('.description h4');
+let prodimg = document.querySelectorAll('.prod img');
+let cartprodname = document.querySelector('#cart-prod-name');
+let cartprodprice = document.querySelector('#cart-prod-price');
+let cartprodimg = document.querySelector('.items img');
+let cartitems = [];
+let cartprods = document.querySelector('.cart-items');
+
+function updateCartDisplay() {
+    cartprods.innerHTML = '';
+
+    cartitems.forEach(item => {
+        let cartItemElement1 = document.createElement('div');
+        cartItemElement1.classList.add('items');
+        let cartItemElement2 = document.createElement('div');
+        let cartItemElement3 = document.createElement('div');
+        let cartItemElement4 = document.createElement('div');
+
+        cartItemElement2.innerHTML = `<img src="${item.image}" alt="${item.name}" width="90px" height="90px">`;
+        cartItemElement3.innerHTML = `<h5 id="cart-prod-name">${item.name}</h5><h5 id="cart-prod-price">${item.price}</h5><div class="quantity"><i class="fas fa-plus"></i><span class="quantity-value">${item.quantity}</span><i class="fas fa-minus"></i></div>`;
+        cartItemElement4.innerHTML = `<i class="far fa-times-circle fa-lg"></i><h5 id="item-price">totprice</h5>`;
+        cartItemElement1.innerHTML = `<div class="item-img">${cartItemElement2.innerHTML}</div><div class="item-details">${cartItemElement3.innerHTML}</div><div class="item-process">${cartItemElement4.innerHTML}</div>`;
+        cartprods.appendChild(cartItemElement1);
+    });
+
+    // Attach event listeners to plus and minus buttons
+    const plusBtns = document.querySelectorAll('.fa-plus');
+    const minusBtns = document.querySelectorAll('.fa-minus');
+
+    plusBtns.forEach((plusBtn, index) => {
+        plusBtn.addEventListener('click', () => {
+            const quantityValue = plusBtn.parentElement.querySelector('.quantity-value');
+            let currentValue = parseInt(quantityValue.innerText);
+            let updatedValue = currentValue + 1;
+            quantityValue.innerText = updatedValue;
+            cartitems[index].quantity = updatedValue; // Update the quantity in the cartitems array
+        });
+    });
+
+    minusBtns.forEach((minusBtn, index) => {
+        minusBtn.addEventListener('click', () => {
+            const quantityValue = minusBtn.parentElement.querySelector('.quantity-value');
+            let currentValue = parseInt(quantityValue.innerText);
+            if (currentValue > 1) {
+                let updatedValue = currentValue - 1;
+                quantityValue.innerText = updatedValue;
+                cartitems[index].quantity = updatedValue; // Update the quantity in the cartitems array
+            }
+        });
+    });
+}
+
+addTocart.forEach(function (element) {
+    element.addEventListener('click', () => {
+        let index = Array.from(addTocart).indexOf(element);
+        cartprodname.innerHTML = prodname[index].innerHTML;
+        cartprodprice.innerHTML = prodprice[index].innerHTML;
+        cartprodimg.src = prodimg[index].src;
+
+        let selectedItem = {
+            name: prodname[index].innerHTML,
+            price: prodprice[index].innerHTML,
+            image: prodimg[index].src,
+            quantity: 1 // Set the initial quantity to 1
+        }
+
+        cartitems.push(selectedItem);
+        updateCartDisplay();
+    });
+});
 
 
